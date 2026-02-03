9d44c64d03c868239de4d2c947d247d441b078dc
sporhackevskiy <SPorhackevskiy@vgtrk.ru>
2026-02-03 10:06:52 +0300

    Implement logic of feature_flag__update

--- src/apps/feature_flag/logic/facades/feature_flag.py
+++ src/apps/feature_flag/logic/facades/feature_flag.py
@@ -1,12 +1,24 @@
+from __future__ import annotations
+
 from src.apps.feature_flag.dtos import FeatureFlagUpdateDto
-from src.apps.feature_flag.logic.feature_flag import feature_flag__find_by_pk
+from src.apps.feature_flag.logic.interactors.feature_flag import (
+    feature_flag__activated_at_on_changes,
+    feature_flag__find_by_pk_or_raise,
+    feature_flag__has_changes,
+)
 from src.apps.feature_flag.models import FeatureFlag
 from src.server.db import async_session_generator
 
 
-async def feature_flag__update(*, updated_feature_flag: FeatureFlag, update_dto: FeatureFlagUpdateDto):
+async def feature_flag__update(*, updated_feature_flag: FeatureFlag, update_dto: FeatureFlagUpdateDto) -> None:
     async with async_session_generator() as session:
-        current_feature_flag = await feature_flag__find_by_pk(session=session, pk=updated_feature_flag.id)
+        current_feature_flag = await feature_flag__find_by_pk_or_raise(session=session, pk=updated_feature_flag.id)
+        if not feature_flag__has_changes(update_dto=update_dto, current_feature_flag=current_feature_flag):
+            return
+
+        current_feature_flag.activated_at = feature_flag__activated_at_on_changes(
+            current_feature_flag=current_feature_flag, update_dto=update_dto
+        )
 
         session.add(updated_feature_flag)
         await session.commit()
--- src/apps/feature_flag/logic/interactors/feature_flag.py
+++ src/apps/feature_flag/logic/interactors/feature_flag.py
@@ -1,27 +1,36 @@
+from __future__ import annotations
+
 import datetime
+from logging import getLogger
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.apps.common.exceptions import BusinessLogicException
 from src.apps.feature_flag.dtos import FeatureFlagUpdateDto
 from src.apps.feature_flag.logic.feature_flag import feature_flag__find_by_pk
-from logging import getLogger
-
 from src.apps.feature_flag.models import FeatureFlag
 from src.utils.datetime import datetime_now_with_server_tz
 
+
 logger = getLogger(__name__)
 
-async def feature_flag__activated_at_on_change_is_active(
+
+def feature_flag__activated_at_on_changes(
     *, current_feature_flag: FeatureFlag, update_dto: FeatureFlagUpdateDto
 ) -> datetime.datetime | None:
-    if not current_feature_flag:
-        logger.warning(f'Не удалось установить время начала активации флага, т.к. флаг с id={pk} не существует')
-        return None
-
-    if update_dto.is_active:
+    if update_dto.is_active and current_feature_flag.is_active is False:
         return datetime_now_with_server_tz()
     return None
 
 
-async def feature_flag__find_by_pk_or_raise(*, pk: int, session: AsyncSession | None = None):
+async def feature_flag__find_by_pk_or_raise(*, pk: int, session: AsyncSession | None = None) -> FeatureFlag:
     feature_flag = await feature_flag__find_by_pk(session=session, pk=pk)
+    if not feature_flag:
+        raise BusinessLogicException(f"Данная операция не возможна. не существует FeatureFlag с id={pk}")
+    return feature_flag
+
+
+def feature_flag__has_changes(*, current_feature_flag: FeatureFlag, update_dto: FeatureFlagUpdateDto) -> bool:
+    return any(
+        [current_feature_flag.is_active != update_dto.is_active, current_feature_flag.ttl_days != update_dto.ttl_days]
+    )
