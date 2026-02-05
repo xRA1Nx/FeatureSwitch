32ea91f09e153cc45f6436d8b1bd7f5774d39f48
sporhackevskiy <SPorhackevskiy@vgtrk.ru>
2026-02-05 15:45:59 +0300

    Add tests for feature_flag__activated_at_on_changes

---
+++ src/apps/feature_flag/logic/tests/test_unit/test_logic/test_interactors/conftest.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+
+if TYPE_CHECKING:
+    from unittest.mock import Mock
+
+
+MODULE_NAME = "src.apps.feature_flag.logic.interactors.feature_flag"
+
+
+@pytest.fixture
+def mocked__feature_flag__should_change_activated_at_on_update(mock_for_module) -> Mock:
+    return mock_for_module(module_name=MODULE_NAME, function_name="feature_flag__should_change_activated_at_on_update")
+
+
+@pytest.fixture
+def mocked__datetime_now_with_server_tz(mock_for_module) -> Mock:
+    return mock_for_module(module_name=MODULE_NAME, function_name="datetime_now_with_server_tz")
