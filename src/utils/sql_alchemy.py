5511f30398a61762a7b891ae04f724c72331876b
sporhackevskiy <SPorhackevskiy@vgtrk.ru>
2026-05-06 17:17:32 +0300

    Update utils

--- src/utils/sql_alchemy.py
+++ src/utils/sql_alchemy.py
@@ -37,26 +37,54 @@ async def _add_all_to_session_and_commit(*, session: AsyncSession, instances: li
     await session.commit()
 
 
-def _instance__prepare_for_update(*, instance: BaseModelType, dto: BaseDtoType) -> BaseModelType:
+def _instance__prepare_for_update(*, instance: BaseModelType, dto: BaseDtoType) -> BaseModelType | None:
+    have_changes = False
     for field, value in dto.model_dump(exclude_unset=True).items():
-        if hasattr(instance, field):
+        if hasattr(instance, field) and getattr(instance, field) != value:
             setattr(instance, field, value)
+            have_changes = True
+    if not have_changes:
+        return None
     return instance
 
 
-async def save_model_instance(*, instance: BaseModelType, session: AsyncSession | None = None) -> None:
-    return await save_model_instances(instances=[instance], session=session)
-
-
-async def save_model_instances(*, instances: list[BaseModelType], session: AsyncSession | None = None) -> None:
-    async with optional_session_generator(session=session) as generator_session:
-        await _add_all_to_session_and_commit(session=generator_session, instances=instances)
-
-
-async def update_model_instance(*, instance: BaseModelType, dto: BaseDtoType, session: AsyncSession) -> BaseModelType:
-    instance = _instance__prepare_for_update(instance=instance, dto=dto)
-    await _add_all_to_session_and_commit(session=session, instances=[instance])
+async def save_model_instance[T: BaseModel](*, instance: T, session: AsyncSession | None = None) -> T:
+    await save_model_instances(instances=[instance], session=session)
     return instance
+
+
+async def save_model_instances[T: BaseModel](*, instances: list[T], session: AsyncSession | None = None) -> list[T]:
+    should_commit = True
+    if session:
+        should_commit = False
+
+    async with optional_session_generator(session=session) as async_session:
+        async_session.add_all(instances)
+        if should_commit:
+            await async_session.commit()
+        else:
+            await async_session.flush()
+    return instances
+
+
+async def update_model_instance(
+    *, instance: BaseModelType, dto: BaseDtoType, session: AsyncSession | None = None
+) -> BaseModelType:
+    should_commit = True
+    if session:
+        should_commit = False
+
+    async with optional_session_generator(session=session) as async_session:
+        updated_instance = _instance__prepare_for_update(instance=instance, dto=dto)
+        if updated_instance is None:
+            return instance
+
+        async_session.add(updated_instance)
+        if should_commit:
+            await async_session.commit()
+        else:
+            await async_session.flush()
+    return updated_instance
 
 
 async def delete_model_instance(*, instance: BaseModelType, session: AsyncSession) -> None:
@@ -64,7 +92,7 @@ async def delete_model_instance(*, instance: BaseModelType, session: AsyncSessio
     await session.commit()
 
 
-def if_related_instances_loaded(*, instance: BaseModelType, relationship_name: str) -> bool:
+def is_related_instances_loaded(*, instance: BaseModelType, relationship_name: str) -> bool:
     """Проверяет, загружены ли связанные данные"""
     inspector = inspect(instance)
     return relationship_name not in inspector.unloaded
